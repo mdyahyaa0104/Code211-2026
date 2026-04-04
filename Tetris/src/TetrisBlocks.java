bc80ed27a0a8357bffad3926cb03ec5a2e41231c
GauravMalik1 <Gaurav.the.Malik@gmail.com>
2026-04-04 09:44:38 -0500

    Update TetrisBlocks.java

--- Tetris/src/TetrisBlocks.java
+++ Tetris/src/TetrisBlocks.java
@@ -1,3 +1,11 @@
-public class TetrisBlocks {
+import java.awt.*;
 
+public class TetrisBlocks {
+    private int x, y; // Current grid position
+    private int[][] shape; // 2D array representing the piece
+    private Color color;
+
+    public void moveDown() {
+        y++; // Increment Y to move the block down the grid
+    }
 }
